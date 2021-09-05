9df6641a558470c8b5d13b336fc8927bd87d6953
Joshua Petitma <joshua@petit.dev>
2021-09-05 07:37:34 -0400

    Rename navigation to navigationBar

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -4,7 +4,7 @@ import styles from "@styles/Navigation.module.css";
 const Navigation: NextPage = () => {
   return (
     <div className={styles.imgBanner}>
-      <div className={`primary ${styles.navigation}`}>
+      <div className={`primary ${styles.navigationBar}`}>
         <NavButton title="Assignments" link="/index.html" />
         <NavButton title="Home" link="https://petit.dev" />
         <NavButton title="Final Project" link="/index.html" />
